a07a0749bccddecf0fb7f3314dbb3a8401609278
zoriaxxx <Vasi_co33@student.itstep.org>
2025-01-25 21:37:22 +0200

    DZ_6

---
+++ DZ_7.py
@@ -0,0 +1,26 @@
+def error_handling_decorator(func):
+    def wrapper(expression):
+        try:
+            result = func(expression)
+            print(f"Результат: {result}")
+            return result
+        except ZeroDivisionError:
+            print("Помилка: Ділення на нуль не дозволено.")
+        except SyntaxError:
+            print("Помилка: Невірний синтаксис виразу.")
+        except NameError:
+            print("Помилка: Вираз містить недопустимі символи або змінні.")
+        except Exception as e:
+            print(f"Несподівана помилка: {e}")
+    return wrapper
+
+@error_handling_decorator
+def calculate(expression):
+    return eval(expression)
+
+# Приклади використання
+calculate("5 + 5")
+calculate("10 / 0")
+calculate("5 + ")
+calculate("abc + 5")
+calculate("10 * 2")
