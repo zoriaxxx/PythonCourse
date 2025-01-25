c3a3a29174faca9a2dc082b89f3ff374f0ef70cc
zoriaxxx <Vasi_co33@student.itstep.org>
2025-01-25 19:54:36 +0200

    DZ_1

---
+++ DZ_2.py
@@ -0,0 +1,34 @@
+class Dog:
+    print("ГАВВВ")
+    def __init__(self, name):
+        self.name = name
+        self.age = age
+        self.breed = breed
+        self.weight = 4  # (1,5kg - 140kg)
+        self.energy = 80 # (0-100)
+
+        def eat(self, food)
+            if self.hunger < 100:
+                self.hunger -= 20
+                self.energy += 10
+                print(f"{self.name} I ate, and now I am not hungry anymore!")
+            else:
+                print(f"{self.name} I don't hungry")
+
+        def play(self)
+            if self.energy > 20:
+                self.energy -= 20
+                self.happiness += 20
+                print(f"{self.name} I played, and now I am not hungry anymore!")
+            else:
+                print(f"{self.name} I don't want play")
+
+        def sleep(self)
+                self.energy = 100
+                self.happiness += 20
+                print(f"{self.name} I slept, and now I don't want to sleep!")
+            else:
+                print(f"{self.name} I don't want play")
+
+        def bark(self)
+                print(f"{self.name} woof woof")
